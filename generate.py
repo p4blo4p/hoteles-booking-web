8abd99167396c897a77446f35ba278f2d1bca674
p4blo4p <87369479+p4blo4p@users.noreply.github.com>
2025-09-26 12:57:23 +0200

    Update generate.py

--- generate.py
+++ generate.py
@@ -27,7 +27,7 @@ for dir_path in [TEMPLATES_DIR, STATIC_DIR, DATA_DIR]:
 env = Environment(loader=FileSystemLoader(TEMPLATES_DIR))
 
 # Cargar datos de hoteles
-hoteles_json_path = os.path.join(DATA_DIR, 'hoteles.json')
+hoteles_json_path = os.path.join(DATA_DIR, 'hotels.json')
 print(f"Buscando archivo de hoteles en: {hoteles_json_path}")
 
 if not os.path.exists(hoteles_json_path):
